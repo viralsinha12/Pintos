27ddc55491cbabc2ef6576ecbb5276bd819e2b28
Viral Sinha <viral.sinha@hotmail.com>
2018-10-21 20:57:35 -0400

    comment

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -17,6 +17,7 @@ void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
 void sema_up (struct semaphore *);
 void sema_self_test (void);
+//added for mlfqs_block
 extern bool thread_mlfqs;
 /* Lock. */
 struct lock 
